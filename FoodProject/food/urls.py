086e01717d4341cbc19a3a2693639fbdac0a1e6c
Traianik <traianparii@mail.ru>
2022-12-30 15:52:47 +0200

    Updated list with food

--- FoodProject/food/urls.py
+++ FoodProject/food/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('', views.index, name='index'),
+urlpatterns = [ 
+    path('', views.food_page, name='food_page'),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
  
--- FoodProject/food/views.py
+++ FoodProject/food/views.py
@@ -1,20 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.template import loader
 from random import choice
 from .models import FoodData
-
+from django.contrib.auth.forms import UserCreationForm
 
 def generate_food():
- 
-
+        #get random value from db
         pks = FoodData.objects.values_list('pk', flat=True)
         random_pk = choice(pks)
         random_obj = FoodData.objects.get(pk=random_pk)
         print(random_obj)
+
+        #making sure that is not repeating
+
         return random_obj
 
- 
-def index(request):
+def food_page(request):
         random_food = generate_food()
         context = {'random_food':random_food}   
-        return render(request,'food/index.html',context)
+        return render(request,'food/food_page.html',context)
